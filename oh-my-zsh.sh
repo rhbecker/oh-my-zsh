2aa9cbc5e33b4c37d67c56d1066070fc22a0d773
RH Becker <dev@rhbecker.info>
2014-12-03 18:01:11 -0800

    Load custom configs from $ZSH_CUSTOM before loading plugins defined in ~/.zshrc such that setting of custom zstyles can be done in the "normal" customization place.
    See issue #3282 on oh-my-zsh upstream: https://github.com/robbyrussell/oh-my-zsh/issues/3282

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -54,6 +54,12 @@ fi
 autoload -U compinit
 compinit -i -d "${ZSH_COMPDUMP}"
 
+# Load all of your custom configurations from custom/
+for config_file ($ZSH_CUSTOM/*.zsh(N)); do
+  source $config_file
+done
+unset config_file
+
 # Load all of the plugins that were defined in ~/.zshrc
 for plugin ($plugins); do
   if [ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]; then
@@ -62,12 +68,6 @@ for plugin ($plugins); do
     source $ZSH/plugins/$plugin/$plugin.plugin.zsh
   fi
 done
-
-# Load all of your custom configurations from custom/
-for config_file ($ZSH_CUSTOM/*.zsh(N)); do
-  source $config_file
-done
-unset config_file
 
 # Load the theme
 if [ "$ZSH_THEME" = "random" ]; then
